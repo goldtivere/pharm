db7e82c2979a9f5f799331344c3bab68de307221
Gold <Gold@Gold>
2017-10-20 13:23:10 +0100

    topPayNg

--- src/java/com/pharm/inventory/Vendor.java
+++ src/java/com/pharm/inventory/Vendor.java
@@ -42,8 +42,26 @@ public class Vendor implements Serializable {
     private String vendor_bank;
     private String vendor_acct;
     private Boolean poss;
+    private Boolean status;
+    private TableModel selectedList;
     private List<TableModel> selectList;
     private List<TableModel> vendorList;
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public TableModel getSelectedList() {
+        return selectedList;
+    }
+
+    public void setSelectedList(TableModel selectedList) {
+        this.selectedList = selectedList;
+    }
 
     public List<TableModel> getSelectList() {
         return selectList;
@@ -146,10 +164,20 @@ public class Vendor implements Serializable {
         try {
             vendorList = vendorTable();
             bankmodel = bankModels();
+            status = detectStatus();
             poss = false;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    //detect the rolePriviledge of the logged in user
+    public Boolean detectStatus() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        UserDetails userObj = (UserDetails) context.getExternalContext().getSessionMap().get("sessn_nums");
+        String stat = String.valueOf(userObj.getRole_id());
+
+        return stat.equalsIgnoreCase("admin");
     }
 
     public List<BankModel> bankModels() throws Exception {
@@ -196,6 +224,7 @@ public class Vendor implements Serializable {
         }
     }
 
+    //refreshes vendor registration form
     public void refresh() {
         setVendor_name("");
         setVendor_phone("");
@@ -232,9 +261,10 @@ public class Vendor implements Serializable {
             con = dbConnections.mySqlDBconnection();
 
             // test to see if vendor is already registered
-            String testVenName = "Select * from vendor_table where vendor_name=?";
+            String testVenName = "Select * from vendor_table where vendor_name=? and isdeleted=?";
             pstmt = con.prepareStatement(testVenName);
             pstmt.setString(1, getVendor_name());
+            pstmt.setBoolean(2, false);
             rs = pstmt.executeQuery();
             if (rs.next()) {
                 setMessangerOfTruth("Vendor Name Aleady exists!!");
@@ -254,38 +284,8 @@ public class Vendor implements Serializable {
                         int vendor_id = count + 1;
                         String vendor = "VEN" + String.valueOf(vendor_id);
                         String insert = "insert into vendor_table (vendor_id,vendor_name,vendor_phone,vendor_email,vendor_contact,vendor_bank,vendor_acct_num,"
-                                + "created_by,role_id,date_created,date_time_created,Role) "
-                                + "values(?,?,?,?,?,?,?,?,?,?,?,?)";
-
-                        pstmt = con.prepareStatement(insert);
-
-                        pstmt.setString(1, vendor);
-                        pstmt.setString(2, getVendor_name());
-                        pstmt.setString(3, getVendor_phone());
-                        pstmt.setString(4, getVendor_email());
-                        pstmt.setString(5, getVendor_address());
-                        pstmt.setString(6, bankmode.getBankName());
-                        pstmt.setString(7, getVendor_acct());
-                        pstmt.setString(8, createdby);
-                        pstmt.setString(9, roleId);
-                        pstmt.setString(10, DateManipulation.dateAlone());
-                        pstmt.setString(11, DateManipulation.dateAndTime());
-                        pstmt.setString(12, createdId);
-
-                        pstmt.executeUpdate();
-
-                        setMessangerOfTruth("Vendor Created!!");
-                        vendorList = vendorTable();
-                        msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
-                        context.addMessage(null, msg);
-                        refresh();
-
-                    } else {
-                        int vendor_id = count + 1;
-                        String vendor = "VEN" + String.valueOf(vendor_id);
-                        String insert = "insert into vendor_table (vendor_id,vendor_name,vendor_phone,vendor_email,vendor_contact,vendor_bank,vendor_acct_num,"
-                                + "created_by,role_id,date_created,date_time_created,Role,updatedBy) "
-                                + "values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                                + "created_by,role_id,date_created,date_time_created,Role,updatedBy,isdeleted) "
+                                + "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
                         pstmt = con.prepareStatement(insert);
 
@@ -302,10 +302,43 @@ public class Vendor implements Serializable {
                         pstmt.setString(11, DateManipulation.dateAndTime());
                         pstmt.setString(12, createdId);
                         pstmt.setString(13, createdby);
+                        pstmt.setBoolean(14, false);
 
                         pstmt.executeUpdate();
 
-                        setMessangerOfTruth("Ve Created!!");
+                        setMessangerOfTruth("Vendor Created!!");
+                        vendorList = vendorTable();
+                        msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+                        context.addMessage(null, msg);
+                        refresh();
+
+                    } else {
+                        int vendor_id = count + 1;
+                        String vendor = "VEN" + String.valueOf(vendor_id);
+                        String insert = "insert into vendor_table (vendor_id,vendor_name,vendor_phone,vendor_email,vendor_contact,vendor_bank,vendor_acct_num,"
+                                + "created_by,role_id,date_created,date_time_created,Role,updatedBy,isdeleted) "
+                                + "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+                        pstmt = con.prepareStatement(insert);
+
+                        pstmt.setString(1, vendor);
+                        pstmt.setString(2, getVendor_name());
+                        pstmt.setString(3, getVendor_phone());
+                        pstmt.setString(4, getVendor_email());
+                        pstmt.setString(5, getVendor_address());
+                        pstmt.setString(6, bankmode.getBankName());
+                        pstmt.setString(7, getVendor_acct());
+                        pstmt.setString(8, createdby);
+                        pstmt.setString(9, roleId);
+                        pstmt.setString(10, DateManipulation.dateAlone());
+                        pstmt.setString(11, DateManipulation.dateAndTime());
+                        pstmt.setString(12, createdId);
+                        pstmt.setString(13, createdby);
+                        pstmt.setBoolean(14, false);
+
+                        pstmt.executeUpdate();
+
+                        setMessangerOfTruth("Vendor Created!!");
                         vendorList = vendorTable();
                         msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
                         context.addMessage(null, msg);
@@ -347,8 +380,9 @@ public class Vendor implements Serializable {
         try {
 
             con = dbConnections.mySqlDBconnection();
-            String query = "SELECT * FROM vendor_table order by id desc";
+            String query = "SELECT * FROM vendor_table where isdeleted=? order by id desc";
             pstmt = con.prepareStatement(query);
+            pstmt.setString(1, "true");
             rs = pstmt.executeQuery();
             //
             List<TableModel> lst = new ArrayList<>();
@@ -412,16 +446,41 @@ public class Vendor implements Serializable {
         try {
             con = dbConnections.mySqlDBconnection();
 
-            String testname = "select vendor_Id,count(vendor_name) as total from pharm_db.vendor_table where vendor_name=?";
+            String testname = "select vendor_Id,count(vendor_name) as total from pharm_db.vendor_table where vendor_name=? and isdeleted=?";
             pstmt = con.prepareStatement(testname);
             pstmt.setString(1, ((TableModel) event.getObject()).getVendorName());
+            pstmt.setBoolean(2, false);
             rs = pstmt.executeQuery();
 
             while (rs.next()) {
                 int count = rs.getInt("total");
                 String venId = rs.getString("vendor_Id");
 
-                if (count <= 1 && venId.equals(((TableModel) event.getObject()).getVendorId())) {
+                if (count <= 1 && venId == null) {
+                    String update = "update pharm_db.vendor_table set vendor_name=?,vendor_phone=?,"
+                            + "vendor_email=?,vendor_contact=?,"
+                            + "vendor_bank=?,vendor_acct_num=?,Role=?,updatedBy=?,DateUpdated=? where vendor_Id=?";
+
+                    pstmt = con.prepareStatement(update);
+
+                    pstmt.setString(1, ((TableModel) event.getObject()).getVendorName());
+                    pstmt.setString(2, ((TableModel) event.getObject()).getVendorPnum());
+                    pstmt.setString(3, ((TableModel) event.getObject()).getVendorEmail());
+                    pstmt.setString(4, ((TableModel) event.getObject()).getVendorContact());
+                    pstmt.setString(5, ((TableModel) event.getObject()).getVendorBank());
+                    pstmt.setString(6, ((TableModel) event.getObject()).getVendorAcct());
+                    pstmt.setString(7, roleId);
+                    pstmt.setString(8, createdby);
+                    pstmt.setString(9, DateManipulation.dateAndTime());
+                    pstmt.setString(10, ((TableModel) event.getObject()).getVendorId());
+
+                    pstmt.executeUpdate();
+
+                    setMessangerOfTruth("Vendor " + ((TableModel) event.getObject()).getVendorId() + " Updated!!");
+                    msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+                    context.addMessage(null, msg);
+                    refresh();
+                } else if (count <= 1 && venId.equals(((TableModel) event.getObject()).getVendorId())) {
                     String update = "update pharm_db.vendor_table set vendor_name=?,vendor_phone=?,"
                             + "vendor_email=?,vendor_contact=?,"
                             + "vendor_bank=?,vendor_acct_num=?,Role=?,updatedBy=?,DateUpdated=? where vendor_Id=?";
@@ -455,7 +514,7 @@ public class Vendor implements Serializable {
             }
         } catch (Exception e) {
             setMessangerOfTruth("Error!!");
-            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, String.valueOf(e), String.valueOf(e));
             context.addMessage(null, msg);
             e.printStackTrace();
 
@@ -473,11 +532,6 @@ public class Vendor implements Serializable {
         }
     }
 
-    public void onRowEdits(RowEditEvent event) {
-        FacesMessage msg = new FacesMessage("Vendor Edited" + ((TableModel) event.getObject()).getVendorId(), ((TableModel) event.getObject()).getVendorId());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
     public void onRowCancel(RowEditEvent event) {
         FacesMessage msg = new FacesMessage("Edit Cancelled", ((TableModel) event.getObject()).getVendorId());
         FacesContext.getCurrentInstance().addMessage(null, msg);
@@ -490,6 +544,46 @@ public class Vendor implements Serializable {
         if (newValue != null && !newValue.equals(oldValue)) {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
             FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public void deleteCar() throws Exception {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        DbConnectionX dbConnections = new DbConnectionX();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        FacesMessage msg;
+        UserDetails userObj = (UserDetails) context.getExternalContext().getSessionMap().get("sessn_nums");
+        String on = String.valueOf(userObj);
+
+        if (userObj == null) {
+            setMessangerOfTruth("Expired Session, please re-login" + on);
+            msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, getMessangerOfTruth(), getMessangerOfTruth());
+            context.addMessage(null, msg);
+        }
+
+        try {
+            con = dbConnections.mySqlDBconnection();
+
+            // delete registered vendor
+            String Vendelete = "update vendor_table set isDeleted=?";
+            pstmt = con.prepareStatement(Vendelete);
+            pstmt.setBoolean(1, true);
+            pstmt.executeUpdate();
+            vendorList = vendorTable();
+            setMessangerOfTruth("Vendor " + selectedList.getVendorId() + " deleted!!");
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, getMessangerOfTruth(), getMessangerOfTruth());
+            context.addMessage(null, msg);
+
+        } catch (Exception e) {
+            setMessangerOfTruth("Error!!");
+            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, String.valueOf(e), String.valueOf(e));
+            context.addMessage(null, msg);
+            e.printStackTrace();
+
         }
     }
 
